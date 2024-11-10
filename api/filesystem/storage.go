c168ae912330336571b22ef5c4d78c0b981c2752
Jan540 <naj.schaefer@gmail.com>
2024-11-10 11:52:09 +0100

    if > 5 backups delete oldest

--- api/filesystem/storage.go
+++ api/filesystem/storage.go
@@ -54,43 +54,93 @@ func (ss *SaveStorage) ListFiles() ([]models.SaveFile, error) {
 
 func (ss *SaveStorage) SaveSaves(userId string, saves []*multipart.FileHeader) error {
 	for _, save := range saves {
-		gameCode := save.Filename
+		if err := ss.SaveSave(userId, save); err != nil {
+			return err
+		}
+	}
 
-		src, err := save.Open()
+	return nil
+}
+
+func (ss *SaveStorage) SaveSave(userId string, save *multipart.FileHeader) error {
+	gameCode := save.Filename
+
+	src, err := save.Open()
+	if err != nil {
+		return err
+	}
+	defer src.Close()
+
+	gameDir := filepath.Join(ss.baseDir, userId, gameCode)
+
+	if err := os.MkdirAll(gameDir, os.ModePerm); err != nil {
+		return err
+	}
+
+	dstPath := filepath.Join(gameDir, "current.sav")
+
+	if _, err := os.Stat(dstPath); err == nil {
+		backupFileName := fmt.Sprintf("backup%s.sav", time.Now().Format("060102-150405"))
+
+		backupPath := filepath.Join(gameDir, backupFileName)
+
+		if err := os.Rename(dstPath, backupPath); err != nil {
+			return err
+		}
+
+		files, err := os.ReadDir(gameDir)
 		if err != nil {
 			return err
 		}
-		defer src.Close()
 
-		gameDir := filepath.Join(ss.baseDir, userId, gameCode)
-
-		if err := os.MkdirAll(gameDir, os.ModePerm); err != nil {
-			return err
-		}
-
-		dstPath := filepath.Join(gameDir, "current.sav")
-
-		if _, err := os.Stat(dstPath); err == nil {
-			backupFileName := fmt.Sprintf("backup%s.sav", time.Now().Format("060102-150405"))
-
-			backupPath := filepath.Join(gameDir, backupFileName)
-
-			if err := os.Rename(dstPath, backupPath); err != nil {
+		if len(files) > 5 {
+			if err := ss.deleteOldestSave(gameDir, files); err != nil {
 				return err
 			}
+		}
+	}
 
-			// TODO: if more than 5 backups, delete the oldest
+	dst, err := os.Create(dstPath)
+	if err != nil {
+		return err
+	}
+	defer dst.Close()
+
+	if _, err := io.Copy(dst, src); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (ss *SaveStorage) deleteOldestSave(gameDir string, files []os.DirEntry) error {
+	var oldest os.FileInfo
+
+	for _, file := range files {
+		if file.IsDir() {
+			continue
 		}
 
-		dst, err := os.Create(dstPath)
+		info, err := file.Info()
 		if err != nil {
 			return err
 		}
-		defer dst.Close()
 
-		if _, err := io.Copy(dst, src); err != nil {
-			return err
+		if oldest == nil {
+			oldest = info
+			continue
 		}
+
+		fileInfo, _ := file.Info()
+
+		if fileInfo.ModTime().Before(oldest.ModTime()) {
+			oldest = info
+		}
+	}
+
+	err := os.Remove(filepath.Join(gameDir, oldest.Name()))
+	if err != nil {
+		return err
 	}
 
 	return nil
